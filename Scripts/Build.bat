0acec493169cd09c4e15ce3c1d3f3d2721a0179f
LINSYS TECHNOLOGIES <jlinisa@linsys-technologies.fr>
2024-01-13 07:47:30 +0100

    Remplace My computer par Poste de Travail (Version Française LabVIEW)

--- Scripts/Build.bat
+++ Scripts/Build.bat
@@ -1,3 +1,3 @@
 SET cur_dir=%cd%
 echo %cur_dir%
-LabVIEWCLI -LogToConsole true -OperationName ExecuteBuildSpec -ProjectPath "%cur_dir%\Test Pipeline Jenkins.lvproj" -BuildSpecName "Add_Application"
+LabVIEWCLI -LogToConsole true -OperationName ExecuteBuildSpec -ProjectPath "%cur_dir%\Test Pipeline Jenkins.lvproj" -BuildSpecName "Add_Application" -TargetName "Poste de travail"
